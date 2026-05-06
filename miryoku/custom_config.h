eee6978511decf8c846418706ebaa28b2a587173
GofranChang <36130357+GofranChang@users.noreply.github.com>
2026-05-06 22:49:08 +0800

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,85 +2,76 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALPHAS_QWERTY
-
 #define MIRYOKU_NAV_VI
 
-/* Base 层中间六个键：方向键 */
-#define EYELASH_E00_BASE &kp UP
-#define EYELASH_E01_BASE &kp LEFT
-#define EYELASH_E02_BASE &none
-#define EYELASH_E03_BASE &kp RIGHT
-#define EYELASH_E04_BASE &none
-#define EYELASH_E05_BASE &kp DOWN
+/* 所有层 E00/E01 空置，E02/E03 Windows 桌面切换，E04 复制，E05 粘贴 */
 
-/* Extra 层：鼠标移动 */
-#define EYELASH_E00_EXTRA &mmv MOVE_UP
-#define EYELASH_E01_EXTRA &mmv MOVE_LEFT
-#define EYELASH_E02_EXTRA &none
-#define EYELASH_E03_EXTRA &mmv MOVE_RIGHT
-#define EYELASH_E04_EXTRA &none
-#define EYELASH_E05_EXTRA &mmv MOVE_DOWN
+#define EYELASH_E00_BASE &none
+#define EYELASH_E01_BASE &none
+#define EYELASH_E02_BASE &kp LC(LG(LEFT))
+#define EYELASH_E03_BASE &kp LC(LG(RIGHT))
+#define EYELASH_E04_BASE &kp LC(C)
+#define EYELASH_E05_BASE &kp LC(V)
 
-/* Tap 层：鼠标移动 */
-#define EYELASH_E00_TAP &mmv MOVE_UP
-#define EYELASH_E01_TAP &mmv MOVE_LEFT
-#define EYELASH_E02_TAP &none
-#define EYELASH_E03_TAP &mmv MOVE_RIGHT
-#define EYELASH_E04_TAP &none
-#define EYELASH_E05_TAP &mmv MOVE_DOWN
+#define EYELASH_E00_EXTRA &none
+#define EYELASH_E01_EXTRA &none
+#define EYELASH_E02_EXTRA &kp LC(LG(LEFT))
+#define EYELASH_E03_EXTRA &kp LC(LG(RIGHT))
+#define EYELASH_E04_EXTRA &kp LC(C)
+#define EYELASH_E05_EXTRA &kp LC(V)
 
-/* Button 层：鼠标移动 */
-#define EYELASH_E00_BUTTON &mmv MOVE_UP
-#define EYELASH_E01_BUTTON &mmv MOVE_LEFT
-#define EYELASH_E02_BUTTON &none
-#define EYELASH_E03_BUTTON &mmv MOVE_RIGHT
-#define EYELASH_E04_BUTTON &none
-#define EYELASH_E05_BUTTON &mmv MOVE_DOWN
+#define EYELASH_E00_TAP &none
+#define EYELASH_E01_TAP &none
+#define EYELASH_E02_TAP &kp LC(LG(LEFT))
+#define EYELASH_E03_TAP &kp LC(LG(RIGHT))
+#define EYELASH_E04_TAP &kp LC(C)
+#define EYELASH_E05_TAP &kp LC(V)
 
-/* Nav 层：鼠标移动 */
-#define EYELASH_E00_NAV &mmv MOVE_UP
-#define EYELASH_E01_NAV &mmv MOVE_LEFT
-#define EYELASH_E02_NAV &none
-#define EYELASH_E03_NAV &mmv MOVE_RIGHT
-#define EYELASH_E04_NAV &none
-#define EYELASH_E05_NAV &mmv MOVE_DOWN
+#define EYELASH_E00_BUTTON &none
+#define EYELASH_E01_BUTTON &none
+#define EYELASH_E02_BUTTON &kp LC(LG(LEFT))
+#define EYELASH_E03_BUTTON &kp LC(LG(RIGHT))
+#define EYELASH_E04_BUTTON &kp LC(C)
+#define EYELASH_E05_BUTTON &kp LC(V)
 
-/* Mouse 层：鼠标移动 */
-#define EYELASH_E00_MOUSE &mmv MOVE_UP
-#define EYELASH_E01_MOUSE &mmv MOVE_LEFT
-#define EYELASH_E02_MOUSE &none
-#define EYELASH_E03_MOUSE &mmv MOVE_RIGHT
-#define EYELASH_E04_MOUSE &none
-#define EYELASH_E05_MOUSE &mmv MOVE_DOWN
+#define EYELASH_E00_NAV &none
+#define EYELASH_E01_NAV &none
+#define EYELASH_E02_NAV &kp LC(LG(LEFT))
+#define EYELASH_E03_NAV &kp LC(LG(RIGHT))
+#define EYELASH_E04_NAV &kp LC(C)
+#define EYELASH_E05_NAV &kp LC(V)
 
-/* Media 层：鼠标移动 */
-#define EYELASH_E00_MEDIA &mmv MOVE_UP
-#define EYELASH_E01_MEDIA &mmv MOVE_LEFT
-#define EYELASH_E02_MEDIA &none
-#define EYELASH_E03_MEDIA &mmv MOVE_RIGHT
-#define EYELASH_E04_MEDIA &none
-#define EYELASH_E05_MEDIA &mmv MOVE_DOWN
+#define EYELASH_E00_MOUSE &none
+#define EYELASH_E01_MOUSE &none
+#define EYELASH_E02_MOUSE &kp LC(LG(LEFT))
+#define EYELASH_E03_MOUSE &kp LC(LG(RIGHT))
+#define EYELASH_E04_MOUSE &kp LC(C)
+#define EYELASH_E05_MOUSE &kp LC(V)
 
-/* Num 层：鼠标移动 */
-#define EYELASH_E00_NUM &mmv MOVE_UP
-#define EYELASH_E01_NUM &mmv MOVE_LEFT
-#define EYELASH_E02_NUM &none
-#define EYELASH_E03_NUM &mmv MOVE_RIGHT
-#define EYELASH_E04_NUM &none
-#define EYELASH_E05_NUM &mmv MOVE_DOWN
+#define EYELASH_E00_MEDIA &none
+#define EYELASH_E01_MEDIA &none
+#define EYELASH_E02_MEDIA &kp LC(LG(LEFT))
+#define EYELASH_E03_MEDIA &kp LC(LG(RIGHT))
+#define EYELASH_E04_MEDIA &kp LC(C)
+#define EYELASH_E05_MEDIA &kp LC(V)
 
-/* Sym 层：鼠标移动 */
-#define EYELASH_E00_SYM &mmv MOVE_UP
-#define EYELASH_E01_SYM &mmv MOVE_LEFT
-#define EYELASH_E02_SYM &none
-#define EYELASH_E03_SYM &mmv MOVE_RIGHT
-#define EYELASH_E04_SYM &none
-#define EYELASH_E05_SYM &mmv MOVE_DOWN
+#define EYELASH_E00_NUM &none
+#define EYELASH_E01_NUM &none
+#define EYELASH_E02_NUM &kp LC(LG(LEFT))
+#define EYELASH_E03_NUM &kp LC(LG(RIGHT))
+#define EYELASH_E04_NUM &kp LC(C)
+#define EYELASH_E05_NUM &kp LC(V)
 
-/* Fun 层：鼠标移动 */
-#define EYELASH_E00_FUN &mmv MOVE_UP
-#define EYELASH_E01_FUN &mmv MOVE_LEFT
-#define EYELASH_E02_FUN &none
-#define EYELASH_E03_FUN &mmv MOVE_RIGHT
-#define EYELASH_E04_FUN &none
-#define EYELASH_E05_FUN &mmv MOVE_DOWN
+#define EYELASH_E00_SYM &none
+#define EYELASH_E01_SYM &none
+#define EYELASH_E02_SYM &kp LC(LG(LEFT))
+#define EYELASH_E03_SYM &kp LC(LG(RIGHT))
+#define EYELASH_E04_SYM &kp LC(C)
+#define EYELASH_E05_SYM &kp LC(V)
+
+#define EYELASH_E00_FUN &none
+#define EYELASH_E01_FUN &none
+#define EYELASH_E02_FUN &kp LC(LG(LEFT))
+#define EYELASH_E03_FUN &kp LC(LG(RIGHT))
+#define EYELASH_E04_FUN &kp LC(C)
+#define EYELASH_E05_FUN &kp LC(V)
