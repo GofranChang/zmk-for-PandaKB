faee0d70f5ef07585d87445deb3fcb79b20e3dc2
zhanggaofan <zhanggaofan0827@gmail.com>
2026-05-06 22:16:36 +0800

    Try use miryoku

---
+++ miryoku/custom_config.h
@@ -0,0 +1,86 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_ALPHAS_QWERTY
+
+#define MIRYOKU_NAV_VI
+
+/* Base 层中间六个键：方向键 */
+#define EYELASH_E00_BASE &kp UP
+#define EYELASH_E01_BASE &kp LEFT
+#define EYELASH_E02_BASE &none
+#define EYELASH_E03_BASE &kp RIGHT
+#define EYELASH_E04_BASE &none
+#define EYELASH_E05_BASE &kp DOWN
+
+/* Extra 层：鼠标移动 */
+#define EYELASH_E00_EXTRA &mmv MOVE_UP
+#define EYELASH_E01_EXTRA &mmv MOVE_LEFT
+#define EYELASH_E02_EXTRA &none
+#define EYELASH_E03_EXTRA &mmv MOVE_RIGHT
+#define EYELASH_E04_EXTRA &none
+#define EYELASH_E05_EXTRA &mmv MOVE_DOWN
+
+/* Tap 层：鼠标移动 */
+#define EYELASH_E00_TAP &mmv MOVE_UP
+#define EYELASH_E01_TAP &mmv MOVE_LEFT
+#define EYELASH_E02_TAP &none
+#define EYELASH_E03_TAP &mmv MOVE_RIGHT
+#define EYELASH_E04_TAP &none
+#define EYELASH_E05_TAP &mmv MOVE_DOWN
+
+/* Button 层：鼠标移动 */
+#define EYELASH_E00_BUTTON &mmv MOVE_UP
+#define EYELASH_E01_BUTTON &mmv MOVE_LEFT
+#define EYELASH_E02_BUTTON &none
+#define EYELASH_E03_BUTTON &mmv MOVE_RIGHT
+#define EYELASH_E04_BUTTON &none
+#define EYELASH_E05_BUTTON &mmv MOVE_DOWN
+
+/* Nav 层：鼠标移动 */
+#define EYELASH_E00_NAV &mmv MOVE_UP
+#define EYELASH_E01_NAV &mmv MOVE_LEFT
+#define EYELASH_E02_NAV &none
+#define EYELASH_E03_NAV &mmv MOVE_RIGHT
+#define EYELASH_E04_NAV &none
+#define EYELASH_E05_NAV &mmv MOVE_DOWN
+
+/* Mouse 层：鼠标移动 */
+#define EYELASH_E00_MOUSE &mmv MOVE_UP
+#define EYELASH_E01_MOUSE &mmv MOVE_LEFT
+#define EYELASH_E02_MOUSE &none
+#define EYELASH_E03_MOUSE &mmv MOVE_RIGHT
+#define EYELASH_E04_MOUSE &none
+#define EYELASH_E05_MOUSE &mmv MOVE_DOWN
+
+/* Media 层：鼠标移动 */
+#define EYELASH_E00_MEDIA &mmv MOVE_UP
+#define EYELASH_E01_MEDIA &mmv MOVE_LEFT
+#define EYELASH_E02_MEDIA &none
+#define EYELASH_E03_MEDIA &mmv MOVE_RIGHT
+#define EYELASH_E04_MEDIA &none
+#define EYELASH_E05_MEDIA &mmv MOVE_DOWN
+
+/* Num 层：鼠标移动 */
+#define EYELASH_E00_NUM &mmv MOVE_UP
+#define EYELASH_E01_NUM &mmv MOVE_LEFT
+#define EYELASH_E02_NUM &none
+#define EYELASH_E03_NUM &mmv MOVE_RIGHT
+#define EYELASH_E04_NUM &none
+#define EYELASH_E05_NUM &mmv MOVE_DOWN
+
+/* Sym 层：鼠标移动 */
+#define EYELASH_E00_SYM &mmv MOVE_UP
+#define EYELASH_E01_SYM &mmv MOVE_LEFT
+#define EYELASH_E02_SYM &none
+#define EYELASH_E03_SYM &mmv MOVE_RIGHT
+#define EYELASH_E04_SYM &none
+#define EYELASH_E05_SYM &mmv MOVE_DOWN
+
+/* Fun 层：鼠标移动 */
+#define EYELASH_E00_FUN &mmv MOVE_UP
+#define EYELASH_E01_FUN &mmv MOVE_LEFT
+#define EYELASH_E02_FUN &none
+#define EYELASH_E03_FUN &mmv MOVE_RIGHT
+#define EYELASH_E04_FUN &none
+#define EYELASH_E05_FUN &mmv MOVE_DOWN
